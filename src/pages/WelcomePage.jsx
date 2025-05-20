394b54af4dc542fe01891ff42ac2b7add3d5d42f
Satyam Singh <satyamrajput7239@gmail.com>
2025-05-20 18:57:36 +0530

    feat: Add Welcome Page

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "../layout/Navbar.jsx";
 import Button from "../components/Button.jsx";
+import women from "../assets/women.png";
+import line from "../assets/line.png";
+import pink from "../assets/redpattern.png";
+import skyblue from "../assets/bluepattern.png";
 import "../styles/WelcomePage.css";
 const WelcomePage = () => {
   return (
@@ -9,9 +13,15 @@ const WelcomePage = () => {
       <div className="hero-page-continer-top">
         <div className="hero-page-container-top-left">
           <div className="hero-page-container-top-left-heading ">
-            Join Our Online
+            Join Our{" "}
+            <div className="online-word">
+              Online{" "}
+              <div>
+                <img src={line} alt="" />
+              </div>
+            </div>
           </div>
-          <div className="hero-page-container-top-left-heading color-pink">
+          <div className="hero-page-container-top-left-heading color-skyblue">
             Classes for Weight Loss, Strength & Self Care
           </div>
           <div className="hero-page-container-top-left-sub-heading">
@@ -29,7 +39,36 @@ const WelcomePage = () => {
         <div className="hero-page-container-top-right"></div>
       </div>
       <div className="hero-page-continer-bottom">
-        <div className="hero-page-continer-bottom-child"></div>
+        <div className="hero-page-container-bottom-child">
+          <div className="tagline-container">
+            <p>Glow into your best self </p>
+          </div>
+          <div className="women-img-container">
+            <img src={women} alt="" />
+          </div>
+          <div className="pattern-pink-container">
+            <img src={pink} alt="" />
+          </div>
+          <div className="pattern-skyblue-container">
+            <img src={skyblue} alt="" />
+          </div>
+          <div className="hero-page-data">
+            <p>Experience</p>
+            <p className="color-pink">5+ years</p>
+          </div>
+          <div className="hero-page-data">
+            <p>Health Coaches</p>
+            <p className="color-pink">20+</p>
+          </div>
+          <div className="hero-page-data">
+            <p>Members</p>
+            <p className="color-pink">500+</p>
+          </div>
+          <div className="hero-page-data">
+            <p>Total Weight Loss</p>
+            <p className="color-pink">2500+ kg</p>
+          </div>
+        </div>
       </div>
     </div>
   );
